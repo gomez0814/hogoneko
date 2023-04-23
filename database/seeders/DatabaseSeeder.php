10c32e24234014313983a51cbb7684f29a5b4899
Matsumoto Ryuho <gomenii.work@gmail.com>
2023-04-23 22:31:45 +0900

    追加：CafeFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+        \App\Models\Cafe::factory(10)->create();
     }
 }
