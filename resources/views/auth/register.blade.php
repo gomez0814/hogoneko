fa9cea3950892932fb12d998b6407a848be2fb51
Matsumoto Ryuho <gomenii.work@gmail.com>
2023-04-23 22:32:17 +0900

    変更：名前ラベル

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,8 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            {{-- <x-input-label for="name" :value="__('Name')" /> --}}
+            <x-input-label for="name" value="アカウント名" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
