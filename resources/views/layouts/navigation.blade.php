a427d04509f9b9d2f52997d60f22af5a47f334e9
Matsumoto Ryuho <gomenii.work@gmail.com>
2023-05-15 00:40:03 +0900

    add: navigationにカフェリスト

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,6 +18,11 @@
                     </x-nav-link>
                 </div>
                 @endauth
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('cafes.list')" :active="request()->routeIs('cafes.list')">
+                        カフェリスト
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -85,6 +90,11 @@
             </x-responsive-nav-link>
         </div>
         @endauth
+        <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('cafes.list')" :active="request()->routeIs('cafes.list')">
+                カフェリスト
+            </x-responsive-nav-link>
+        </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
