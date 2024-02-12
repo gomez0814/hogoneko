de2d7d4f7946fc4f5757452be2cf8a59c10297cf
Matsumoto Ryuho <gomenii.work@gmail.com>
2024-02-13 00:33:50 +0900

    追加/修正：プライバシーポリシーへのルート/ログイン機能のコメントアウト化

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,22 @@ Route::get('/', function () {
     return view('top');
 })->name('top');
 
+Route::get('/privacy', function () {
+    return view('privacy');
+})->name('privacy');
+
 Route::get('/cafes', [CafeController::class, 'list'])->name('cafes.list');
 Route::get('/cafes/{id}', [CafeController::class, 'show'])->name('cafes.show');
 Route::post('/cafes', [CafeController::class, 'listByPrefecture'])->name('cafes.listByPrefecture');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
-require __DIR__.'/auth.php';
+// require __DIR__.'/auth.php';
