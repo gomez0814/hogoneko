bc58b7006cf4c4ce922e3cbbb5dbbea8326f039b
Matsumoto Ryuho <gomenii.work@gmail.com>
2024-02-13 00:30:22 +0900

    修正：説明文

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -10,10 +10,10 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100 leading-10">
                     <p>このサイトは、保護猫カフェを紹介するサイトです。</p>
-                    <p><a href="{{ route('cafes.list') }}" class="text-blue-500 underline">カフェリスト</a>より、お近くの保護猫カフェを探してみてください。</p>
-                    <p>今後はログイン機能を追加し、保護猫カフェのねこちゃんの情報などを掲示板のような形で皆様で共有できればと考えております。
+                    <p><a href="{{ route('cafes.list') }}" class="text-blue-500 underline">保護猫カフェ一覧</a>より、お近くの保護猫カフェを探してみてください。</p>
+                    <p>今後はログイン機能等、新たな機能を追加していきたいと考えております。
                     <p>少しでも保護猫活動をされる皆様のお力になれましたら幸いです。</p>
-                    <p class="text-right">管理人より</p>
+                    <p class="text-right">管理人</p>
                 </div>
             </div>
         </div>
