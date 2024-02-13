1294a4580f0027f16c40bd63778ee4dd40cf7759
Matsumoto Ryuho <gomenii.work@gmail.com>
2024-02-14 00:27:58 +0900

    修正：レスポンシブ対応（テーブルが画面外へはみ出す）

--- resources/views/cafes/show.blade.php
+++ resources/views/cafes/show.blade.php
@@ -25,59 +25,60 @@
                             <img class="w-full mx-auto mb-6" src="{{ $cafe->image_path }}" alt="Image">
                         </div>                        
                     @endif
-
-                    <table class="w-full border">
-                        <tbody>
-                            <tr class="h-16">
-                                <th class="w-32 border">店名</th>
-                                <td class="pl-4 border">{{ $cafe->name }}</td>
-                            </tr>
-                            <tr class="h-16">
-                                <th class="w-32 border">住所</th>
-                                <td class="pl-4 border">{{ $cafe->address }}</td>
-                            </tr>
-                            <tr class="h-16">
-                                <th class="w-32 border">営業時間</th>
-                                <td class="pl-4 border">{{ $cafe->business_hour }}</td>
-                            </tr>
-                            <tr class="h-16">    
-                                <th class="w-32 border">休業日</th>
-                                <td class="pl-4 border">{{ $cafe->holiday }}</td>
-                            </tr>
-                            <tr class="h-16">    
-                                <th class="w-32 border">料金</th>
-                                <td class="pl-4 border">{{ $cafe->charge }}</td>
-                            </tr>
-                            <tr class="h-16">
-                                <th class="w-32 border">情報</th>
-                                <td class="pl-4 border">{{ $cafe->content }}</td>
-                            </tr>
-                            <tr class="h-16">
-                                <th class="w-32 border">Webサイト</th>
-                                <td class="pl-4 border text-blue-500"><a href="{{ $cafe->website }}">{{ $cafe->website }}</td>
-                            </tr>
-                            <tr class="h-16">
-                                <th class="w-32 border">X（旧Twitter）</th>
-                                <td class="pl-4 border text-blue-500"><a href="{{ $cafe->x }}">{{ $cafe->x }}</td>
-                            </tr>
-                            <tr class="h-16">
-                                <th class="w-32 border">Instagram</th>
-                                <td class="pl-4 border text-blue-500"><a href="{{ $cafe->instagram }}">{{ $cafe->instagram }}</td>
-                            </tr>
-                            <tr class="h-16">
-                                <th class="w-32 border">YouTube</th>
-                                <td class="pl-4 border text-blue-500"><a href="{{ $cafe->youtube }}">{{ $cafe->youtube }}</td>
-                            </tr>
-                            <tr class="h-16">
-                                <th class="w-32 border">Amazonほしいものリスト</th>
-                                <td class="pl-4 border text-blue-500"><a href="{{ $cafe->amazon }}">{{ $cafe->amazon }}</td>
-                            </tr>
-                            <tr class="h-16">
-                                <th class="w-32 border">管理者より</th>
-                                <td class="pl-4 border">{{ $cafe->admin }}</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                    <div class="overflow-x-auto">
+                        <table class="w-full border">
+                            <tbody>
+                                <tr class="h-16">
+                                    <th class="px-2 border bg-gray-100">店名</th>
+                                    <td class="px-2 border">{{ $cafe->name }}</td>
+                                </tr>
+                                <tr class="h-16">
+                                    <th class="px-2 border bg-gray-100">住所</th>
+                                    <td class="px-2 border">{{ $cafe->address }}</td>
+                                </tr>
+                                <tr class="h-16">
+                                    <th class="px-2 border bg-gray-100">営業時間</th>
+                                    <td class="px-2 border">{{ $cafe->business_hour }}</td>
+                                </tr>
+                                <tr class="h-16">    
+                                    <th class="px-2 border bg-gray-100">休業日</th>
+                                    <td class="px-2 border">{{ $cafe->holiday }}</td>
+                                </tr>
+                                <tr class="h-16">    
+                                    <th class="px-2 border bg-gray-100">料金</th>
+                                    <td class="px-2 border">{{ $cafe->charge }}</td>
+                                </tr>
+                                <tr class="h-16">
+                                    <th class="px-2 border bg-gray-100">情報</th>
+                                    <td class="px-2 border">{{ $cafe->content }}</td>
+                                </tr>
+                                <tr class="h-16">
+                                    <th class="px-2 border bg-gray-100">Webサイト</th>
+                                    <td class="px-2 border text-blue-500"><a href="{{ $cafe->website }}">{{ $cafe->website }}</td>
+                                </tr>
+                                <tr class="h-16">
+                                    <th class="px-2 border bg-gray-100">X（旧Twitter）</th>
+                                    <td class="px-2 border text-blue-500"><a href="{{ $cafe->x }}">{{ $cafe->x }}</td>
+                                </tr>
+                                <tr class="h-16">
+                                    <th class="px-2 border bg-gray-100">Instagram</th>
+                                    <td class="px-2 border text-blue-500"><a href="{{ $cafe->instagram }}">{{ $cafe->instagram }}</td>
+                                </tr>
+                                <tr class="h-16">
+                                    <th class="px-2 border bg-gray-100">YouTube</th>
+                                    <td class="px-2 border text-blue-500"><a href="{{ $cafe->youtube }}">{{ $cafe->youtube }}</td>
+                                </tr>
+                                <tr class="h-16">
+                                    <th class="px-2 border bg-gray-100">Amazonほしいものリスト</th>
+                                    <td class="px-2 border text-blue-500"><a href="{{ $cafe->amazon }}">{{ $cafe->amazon }}</td>
+                                </tr>
+                                <tr class="h-16">
+                                    <th class="px-2 border bg-gray-100">管理者より</th>
+                                    <td class="px-2 border">{{ $cafe->admin }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                     @if(isset($cafe->map))
                         <div class="flex items-center mt-16">
                             <div class="h-10 w-1 bg-orange-300 mr-4"></div>
