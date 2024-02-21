c4ec57ca232d6dcdc3f8d39b13efb45b4bfe2399
Matsumoto Ryuho <gomenii.work@gmail.com>
2024-02-21 21:43:26 +0900

    追加：カフェリスト表示順条件

--- app/Http/Controllers/CafeController.php
+++ app/Http/Controllers/CafeController.php
@@ -7,23 +7,24 @@ use Illuminate\Http\Request;
 
 class CafeController extends Controller
 {
-    //
+    // 初期表示（全件表示）
     public function list()
     {
-        $cafes = Cafe::paginate(20);
+        $cafes = Cafe::orderBy('name', 'asc')->paginate(20);
         $currentPage = $cafes->currentPage();
 
         return view('cafes.list', compact('cafes', 'currentPage'));
     }
 
+    // 都道府県選択時表示
     public function listByPrefecture(Request $request)
     {
         $prefecture = $request->input('prefecture');
 
         if ($prefecture === "全て") {
-            $cafes = Cafe::paginate(20);
+            $cafes = Cafe::orderBy('name', 'asc')->paginate(20);
         } else {
-            $cafes = Cafe::where('prefecture', $prefecture)->paginate(20);
+            $cafes = Cafe::orderBy('address', 'asc')->where('prefecture', $prefecture)->paginate(20);
         }
         
         $currentPage = $cafes->currentPage();
@@ -31,10 +32,10 @@ class CafeController extends Controller
         return view('cafes.list', compact('prefecture', 'cafes', 'currentPage'));
     }
 
+    // 猫カフェ名リンク押下時
     public function show($id)
     {
         $cafe = Cafe::findOrFail($id);
-        // return view('cafes.show', ['cafe' => $cafe]);
         return view('cafes.show', compact('cafe'));
     }
 }
