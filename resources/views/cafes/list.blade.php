cb3054b1793ed61cfb8e9a44a001a632159c23ca
Matsumoto Ryuho <gomenii.work@gmail.com>
2024-02-23 20:06:18 +0900

    修正：テーブルデザイン、戻るボタン

--- resources/views/cafes/list.blade.php
+++ resources/views/cafes/list.blade.php
@@ -14,7 +14,7 @@
                     @if(isset($prefecture) && count($cafes) !== 0)
                         <div class="flex items-center mb-6">
                             <div class="h-10 w-1 bg-orange-300 mr-4"></div>
-                            <p class="text-2xl font-bold">{{ $prefecture }} の保護猫カフェ一覧</p>
+                            <p class="text-2xl font-bold">{{ $prefecture }}の保護猫カフェ一覧</p>
                         </div>
                         <p>都道府県で検索したい場合は、こちらから選択してください。</p>
                     
@@ -33,7 +33,7 @@
                     
                     <form action="{{ route('cafes.listByPrefecture') }}" method="POST">
                         @csrf
-                        <select class="my-4 dark:bg-gray-800 dark:border-gray-100" name="prefecture" onchange="this.form.submit()">
+                        <select class="my-4 dark:bg-gray-800 dark:border-gray-100 rounded" name="prefecture" onchange="this.form.submit()">
                             <option value="" selected>都道府県を選択</option>
                             <option value="全て">全て</option>
                             <option value="北海道">北海道</option>
@@ -87,32 +87,57 @@
                     </form>
                     @if(count($cafes) !== 0)
                         <p class="mt-8">店名をクリックすると詳細情報ページが表示されます。</p>
-                        <table class="w-full border my-4">
-                            <thead>
-                                <tr>
-                                    <th class="border w-10">No.</th>
-                                    <th class="border w-1/3">店名</th>
-                                    <th class="border">住所</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($cafes as $key => $cafe)
+                        
+                        <!-- PC＆タブレット向け -->
+                        <div class="hidden sm:block">
+                            <table class="my-4">
+                                <thead>
                                     <tr>
-                                        <td class="border text-center">
-                                            @if($currentPage === 1)
-                                                {{ $key + 1}}</td>
-                                            @else
-                                                {{ $currentPage * 10 + $key + 1}}</td>
-                                            @endif
-                                        <td class="border h-16 pl-4 text-blue-500">
-                                            <a href="{{ route('cafes.show', ['id' => $cafe->id]) }}">{{ $cafe->name }}</a></td>
-                                        <td class="border h-16 pl-4">{{ $cafe->address }}</td>
+                                        <th class="text-left bg-orange-50 dark:bg-gray-600 px-4 border-b">店名</th>
+                                        <th class="text-left bg-orange-50 dark:bg-gray-600 px-8 border-b">住所</th>
+                                        <th class="text-left bg-orange-50 dark:bg-gray-600 px-8 border-b">営業時間</th>
                                     </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach ($cafes as $cafe)
+                                        <tr>
+                                            <td class="border-b px-4 py-4 text-blue-500"><a href="{{ route('cafes.show', ['id' => $cafe->id]) }}">{{ $cafe->name }}</a></td>
+                                            <td class="border-b px-8 py-4">{{ $cafe->address }}</td>
+                                            <td class="border-b px-8 py-4"><p class="leading-loose">{!! nl2br(e($cafe->business_hour)) !!}</p></td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+
+                        {{-- スマホ向け --}}
+                        <div class="block sm:hidden">
+                            <table class="my-4">
+                                <thead>
+                                    <tr>
+                                        <th class="text-left bg-orange-50 dark:bg-gray-600 px-2 border-b">店名</th>
+                                        <th class="text-left bg-orange-50 dark:bg-gray-600 px-2 border-b">住所</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($cafes as $cafe)
+                                        <tr>
+                                            <td class="border-b px-2 py-4 text-blue-500"><a href="{{ route('cafes.show', ['id' => $cafe->id]) }}">{{ $cafe->name }}</a></td>
+                                            <td class="border-b px-2 py-4">{{ $cafe->address }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+
+                        {{-- ページネーション --}}
                         {{ $cafes->links() }}
                     @endif
+
+                    {{-- 戻るボタン --}}
+                    <div class="flex justify-center">
+                        <button type="button" class="text-white rounded bg-orange-500 hover:bg-orange-700 px-5 py-2.5 mt-8" onClick="history.back()">戻る</button>
+                    </div>
                 </div>
             </div>
         </div>
