8c07634770fb28166bbcdb4ff29656159c694cdf
Matsumoto Ryuho <gomenii.work@gmail.com>
2024-03-09 21:56:56 +0900

    追加：タイトル

--- resources/views/cafes/show.blade.php
+++ resources/views/cafes/show.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+
+    <x-slot name="title">
+        {{ $cafe->name }}の詳細情報
+    </x-slot>
+    
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             保護猫カフェの詳細情報
--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+
+    <x-slot name="title">
+            プライバシーポリシー
+    </x-slot>
+    
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             プライバシーポリシー
