8e25aac5ee9bff4a1024c27b4b5ea07dc946e99a
Matsumoto Ryuho <gomenii.work@gmail.com>
2024-03-09 21:58:43 +0900

    追加：寄付先一覧, SNS検索ページのルート

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\CafeController;
+use App\Http\Controllers\SnsController;
+use App\Http\Controllers\DonationController;
 use App\Http\Controllers\ProfileController;
+use App\Models\Cafe;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,9 +26,17 @@ Route::get('/privacy', function () {
     return view('privacy');
 })->name('privacy');
 
+// 保護猫カフェ一覧
 Route::get('/cafes', [CafeController::class, 'list'])->name('cafes.list');
 Route::get('/cafes/{id}', [CafeController::class, 'show'])->name('cafes.show');
-Route::post('/cafes', [CafeController::class, 'listByPrefecture'])->name('cafes.listByPrefecture');
+
+// 寄付先一覧
+Route::get('/donation', [DonationController::class, 'list'])->name('donation.list');
+Route::get('/donation/{id}', [CafeController::class, 'show']);
+
+// SNS検索
+Route::get('/sns', [SnsController::class, 'list'])->name('sns.list');
+Route::get('/sns/{id}', [CafeController::class, 'show']);
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
