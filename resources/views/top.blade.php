dd5f946c35bea4686fc7890b997439dee1146701
Matsumoto Ryuho <gomenii.work@gmail.com>
2024-03-11 00:02:30 +0900

    追加：タイトル

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
+
+    <x-slot name="title">
+        トップページ
+    </x-slot>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             トップページ
